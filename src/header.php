b75a35eb17a6c8dc0986d4a6e4708fcff17354f3
Habbe <91habbe@gmail.com>
2018-05-05 16:03:43 +0200

    first commit, base code

---
+++ src/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?= $title ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/main.css" />
+</head>
+<body>
+    <header>
+        <section class="hero is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        Pizza house
+                    </h1>
+                    <h2 class="subtitle">
+                        Company slogan
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <div class="tabs is-centered is-large">
+            <ul>
+                <li class="is-active"><a>Pictures</a></li>
+                <li><a>Music</a></li>
+                <li><a>Videos</a></li>
+                <li><a>Documents</a></li>
+            </ul>
+        </div>
+    </header>
---
+++ src/index.php
@@ -0,0 +1,3 @@
+<?php include_once("./header.php") ?>
+<?php print_r($_SERVER) ?>
+<?php include_once("./footer.php") ?>
